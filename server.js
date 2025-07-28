ca99aee709fc6db8cfab9edb831238b6bfc81597
VincentGriffitheFelicia <feliciavincentgriffithe@gmail.com>
2025-07-28 13:40:23 +0800

    Used JSON file for service account key instead of .env

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const admin = require('firebase-admin')
 const cors = require('cors')
 const { body, validationResult } = require('express-validator')
 
-// Load Firebase credentials from environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+const serviceAccount = require('./serviceAccountKey.json')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
